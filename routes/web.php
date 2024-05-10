7d05e1eb02d9f8f53b029ddf5daca28a2d8f9377
Manuel <manuelloverso60@gmail.com>
2024-05-10 16:11:34 +0200

    add train model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Train;
+use Illuminate\Routing\RouteRegistrar;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    dd(Train::all());
     return view('home');
 });
